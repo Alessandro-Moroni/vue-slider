3ece7cfa860e590bbebba304974550c38e310791
Alessandro-Moroni <alemoro98@gmail.com>
2024-03-12 17:46:23 +0100

    [did]bonus4

--- js/script.js
+++ js/script.js
@@ -33,25 +33,33 @@ createApp({
       },
       ],
       counter: 0,
-      isPlayerAcive: true,
-      isActiveImg: true,
+      isPlayerActive: true,
+      change: true,
     }
   },
 
   methods:{
-    nextPic(isNext){
-      isNext ? this.counter++ : this.counter--;
+    nextPic(isPlayerActive){
+      // isNext ? this.counter++ : this.counter--;
+      
+      if(this.change){
+        isPlayerActive ? this.counter++ : this.conuter--;
+      }else{
+        isPlayerActive ? this.counter-- : this.conuter++;
+        
+      }
 
       if(this.counter === this.images.length){
         this.counter = 0;
       }else if(this.counter < 0){
         this.counter = this.images.length -1;
       }
+     
     },
 
    autoPlay(){
       setInterval( ()=> {
-         if(this.isPlayerAcive){
+         if(this.isPlayerActive){
            this.nextPic(true)
          }
           
